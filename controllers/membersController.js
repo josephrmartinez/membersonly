1e536a7d1166ee2df315cc715fb42d449099515e
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-18 17:37:03 -0700

    Implement sign up process

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -15,23 +15,38 @@ exports.sign_up_get = asyncHandler(async (req, res, next) =>{
     res.render("sign_up_form");
 })
 
-exports.sign_up_post = asyncHandler(async (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        if (err) {
-            return next(err);
-        } else {
-            const member = new Member({
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                username: req.body.username,
-                status: req.body.status,
-                password: hashedPassword
-            });
-            const result = await member.save();
-            res.redirect("/");
+exports.sign_up_post = [
+    body("firstname", "You must provide a first name").trim().isLength({min: 2}).escape(),
+    body("lastname", "You must provide a last name").trim().isLength({min: 2}).escape(),
+    body("username", "You must provide a username").trim().isLength({min: 2}).escape(),
+    body("password", "You must provide a password").trim().isLength({min: 2}).escape(),
+    
+    
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (errors) {
+            console.log(errors)
+            return res.redirect("/")
         }
-        })
-      });
+
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                return next(err);
+            } else {
+                const member = new Member({
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    username: req.body.username,
+                    status: req.body.status,
+                    password: hashedPassword
+                });
+                const result = await member.save();
+                return res.redirect("/");
+                }
+            })
+      })
+    ]
 
 exports.log_in_post = asyncHandler (async (req, res, next) =>{
     passport.authenticate("local", {
